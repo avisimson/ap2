554da7226147b59e9ae8455c0c877005936a94b7
Avi Simson <avi.simson@live.biu.ac.il>
2018-04-08 15:11:44 +0300

    Add project files.

---
+++ ImageService/Server/ImageServer.cs
@@ -0,0 +1,27 @@
+﻿using ImageService.Controller;
+using ImageService.Controller.Handlers;
+using ImageService.Infrastructure.Enums;
+using ImageService.Logging;
+using ImageService.Modal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageService.Server
+{
+    public class ImageServer
+    {
+        //
+        ILoggingService loggingService;
+        ImageController controller;
+
+
+        #region Properties
+        public event EventHandler<CommandRecievedEventArgs> CommandRecieved;          // The event that notifies about a new Command being recieved
+        #endregion
+
+       
+    }
+}
