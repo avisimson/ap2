9f9cbc52f2dea986d5dd9ef752e49a9aec384831
yakir pinchas <yakir.pinchas@live.biu.ac.il>
2018-05-23 19:31:35 +0300

    log xaml

--- GUI/View/ConvertLog.cs
+++ GUI/View/ConvertLog.cs
@@ -9,8 +9,23 @@ using System.Windows.Media;
 
 namespace GUI.View
 {
+    /// <summary>
+    /// converter for the log values
+    /// </summary>
+    /// <seealso cref="System.Windows.Data.IValueConverter" />
     class ConvertLog : IValueConverter
     {
+        /// <summary>
+        /// Converts a value.
+        /// </summary>
+        /// <param name="value">The value produced by the binding source.</param>
+        /// <param name="targetType">The type of the binding target property.</param>
+        /// <param name="parameter">The converter parameter to use.</param>
+        /// <param name="culture">The culture to use in the converter.</param>
+        /// <returns>
+        /// A converted value. If the method returns <see langword="null" />, the valid null value is used.
+        /// </returns>
+        /// <exception cref="InvalidOperationException">This needs to be converted to a brush.</exception>
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             if (targetType.Name != "Brush")
@@ -37,9 +52,19 @@ namespace GUI.View
             }
         }
 
+        /// <summary>
+        /// Converts a value.
+        /// </summary>
+        /// <param name="value">The value that is produced by the binding target.</param>
+        /// <param name="targetType">The type to convert to.</param>
+        /// <param name="parameter">The converter parameter to use.</param>
+        /// <param name="culture">The culture to use in the converter.</param>
+        /// <returns>
+        /// A converted value. If the method returns <see langword="null" />, the valid null value is used.
+        /// </returns>
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            throw new NotImplementedException();
+            return true;
         }
     }
 }
