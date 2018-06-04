9b0237b0fd055c8532ccc530ddb906063e7dc07f
yakir pinchas <yakir.pinchas@live.biu.ac.il>
2018-06-04 14:35:43 +0300

    commit

---
+++ Communication/Client/IClientConnection.cs
@@ -0,0 +1,22 @@
+﻿using Communication;
+using Communication.Event;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GUI.Communication
+{
+    public interface IClientConnection
+    {
+        bool Connect(); //connect to server.
+        void Disconnect(); //disconnect.
+        void Write(CommandReceivedEventArgs e); //write to service a command
+        void Read(); //read back from service
+        event EventHandler<CommandReceivedEventArgs> DataReceived;
+        bool IsConnected { get; set; } //true while the connection to service is on.
+        void Initialize(CommandReceivedEventArgs request); //first command to service.
+    }
+}
---
+++ WEB/Models/ConfigModel.cs
@@ -0,0 +1,49 @@
+﻿using Communication;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ImageServiceWeb.Models
+{
+    public class ConfigModel
+    {
+        public delegate void NotifyAboutChange();
+        private IImageServiceClient client;
+        private List<string> handlers;
+
+        public ConfigModel()
+        {
+            client = ImageServiceClient.Instance;
+            handlers = new List<string>();
+        }
+
+        public List<string> Handlers
+        {
+            get
+            {
+                return this.handlers;
+            }
+        }
+
+
+        [Required]
+        [Display(Name = "Output Directory")]
+        public string OutputDirectory { get; set; }
+
+        [Required]
+        [DataType(DataType.Text)]
+        [Display(Name = "Source Name")]
+        public string SourceName { get; set; }
+
+        [Required]
+        [DataType(DataType.Text)]
+        [Display(Name = "Log Name")]
+        public string LogName { get; set; }
+
+        [Required]
+        [Display(Name = "Thumbnail Size")]
+        public int ThumbnailSize { get; set; }
+    }
+}
